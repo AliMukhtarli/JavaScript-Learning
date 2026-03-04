17ab2f9bc44044514ae2e97617af4d6658d7f103
Əli Muxtarlı <alimukhtarli085@gmail.com>
2026-03-04 10:38:19 +0400

    Outputing opitions

--- learning.js
+++ learning.js
@@ -1,1 +1,12 @@
-// Her dilde olduq
+// Well we know that in many languages if you want to make little simple notes you can do it with just 2 slashes
+
+/* But if you want to make some text or explain some part of code you need to write like this*/
+
+/* Lets talk about outputs. In JavaScript have 4 kind of outputing method. If we use .innerHTML we can write anything in HTML. 
+   Then we can use innerText for showing anything in page.
+   document.write is writing any thing on directly to page
+   window.alert or just "alert" can use for alert something for entering page. It can be microphone, camre or storage permission
+   At last console.log is runing on developer console
+*/
+
+
