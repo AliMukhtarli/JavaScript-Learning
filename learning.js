44566284eb42bcd7bcb1e861d6f88294df685ab6
Əli Muxtarlı <alimukhtarli085@gmail.com>
2026-03-04 12:13:11 +0400

    Data types

--- learning.js
+++ learning.js
@@ -9,4 +9,46 @@
    At last console.log is runing on developer console
 */
 
+// we can declare valubles in 2 form. Const or changeable values. For example.
+
+let q = 5; // it can change on during the runing of the program
+const w = 1; // but its constant
+
+// Also let can be float and double 
+
+let e = 12.23;
+let r = 1234567890132313;
+
+// If you want to declare string with "" or '' . Also you can use double quotes in single and single in double quotes.
+
+let t = "Name";
+let y = 'Surname';
+let u= " Let's go to home";
+let i = 'Did you read J.K.Rowlings "Harry Potter" book? '
+
+// We can define some value and then asign value
+
+let o;
+o = 9;
+
+//For long number JavaScript uses Bigint
+
+let p = 1234567895412987491362074032754507671620378408273601876087695012450832761904075608756047n;
+let a = Bigint(8763217868071650756174560738456176105345235081234567895412987491362074032754507671620378408273601876087695012450832761904075608756047);
+
+/* JavaScript have 8 data types:
+1) String 
+2) Number (float, double, integer)
+3) Biginit
+4) Boolean 
+5) Object (A collection of key-value pairs of data)
+6) Undefined (A primitive variable with no assigned value)
+7) Null (A primitive value representing object absence)
+8) Symbol (A unique and primitive identifier)
+*/
+
+// On JavaScript you can use exponentional notation
+
+let s = 12345e6;
+let d = 12345e-6;
 
